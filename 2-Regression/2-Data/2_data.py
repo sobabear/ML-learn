59d27fa876f853820edd620754d8d0bfa2c9e4a6
Yongjun <stareta1202@gmail.com>
2024-01-21 19:08:46 +0900

    Complete  Data2C

--- 2-Regression/2-Data/2_data.py
+++ 2-Regression/2-Data/2_data.py
@@ -1,7 +1,29 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Use the full path to ensure the file can be found
 pumpkins = pd.read_csv("./2-Regression/2-Data/pumpkins.csv")
-pumpkins.head()
 
+pumpkins = pumpkins[pumpkins['Package'].str.contains('bushel', case=True, regex=True)]
+
+pumpkins.head()
 pumpkins.isnull().sum()
+
+new_columns = ['Package', 'Month', 'Low Price', 'High Price', 'Date']
+pumpkins = pumpkins.drop([c for c in pumpkins.columns if c not in new_columns], axis=1)
+
+price = (pumpkins['Low Price'] + pumpkins['High Price']) / 2
+month = pd.DatetimeIndex(pumpkins['Date']).month
+
+new_pumpkins = pd.DataFrame({'Month': month, 'Package': pumpkins['Package'], 'Low Price': pumpkins['Low Price'],'High Price': pumpkins['High Price'], 'Price': price})
+new_pumpkins.loc[new_pumpkins['Package'].str.contains('1 1/9'), 'Price'] = price/(1 + 1/9)
+
+new_pumpkins.loc[new_pumpkins['Package'].str.contains('1/2'), 'Price'] = price/(1/2)
+
+# print(new_pumpkins)
+
+price = new_pumpkins.Price
+month = new_pumpkins.Month
+
+new_pumpkins.groupby(['Month'])['Price'].mean().plot(kind='bar')
+plt.ylabel('Pumpkin Price')
